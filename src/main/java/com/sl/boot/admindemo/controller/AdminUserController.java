3a9a5e2d724c8ecca6476b51d9c94e0dfa7f2880
lin.shi <lin.shi@tendcloud.com>
2020-03-14 17:33:04 +0800

    删除用户功能实现

--- src/main/java/com/sl/boot/admindemo/controller/AdminUserController.java
+++ src/main/java/com/sl/boot/admindemo/controller/AdminUserController.java
@@ -2,6 +2,7 @@ package com.sl.boot.admindemo.controller;
 
 
 import com.sl.boot.admindemo.dao.AdminUserDAO;
+import com.sl.boot.admindemo.dto.UserDTO;
 import com.sl.boot.admindemo.service.AdminUserService;
 import com.sl.boot.admindemo.vo.UserVo;
 import com.sl.boot.admindemo.vo.resp.BaseResp;
@@ -22,8 +23,19 @@ public class AdminUserController {
         return new BaseResp();
     }
 
-    @DeleteMapping(value = "/delOneUser")
-    public BaseResp delOnerUser(@RequestBody UserVo userVo) {
+    @PostMapping(value = "/delOneUser")
+    public BaseResp delOnerUser(@RequestBody UserDTO userDTO) {
+        UserVo userVo = new UserVo();
+        userVo.setUserName(userDTO.getUserName());
+        if (userDTO.getUserType().equals("管理员")) {
+            userVo.setType(1);
+        } else if (userDTO.getUserType().equals("药库管理员")) {
+            userVo.setType(2);
+        } else if (userDTO.getUserType().equals("医生")) {
+            userVo.setType(3);
+        } else if (userDTO.getUserType().equals("病患")) {
+            userVo.setType(4);
+        }
         adminUserService.delUser(userVo.getUserName(), userVo.getType());
         return new BaseResp();
     }
